5286e7b2531f12880d7b1e1b79d639dd04db2b99
naruto4526 <shakthivarun007@gmail.com>
2024-01-26 09:03:12 +0530

    added List of reminders

--- components/reminders/Reminders.jsx
+++ components/reminders/Reminders.jsx
@@ -1,26 +1,50 @@
-import React,{useState} from "react";
-import {View,Text,TouchableOpacity, Button} from 'react-native';
+import React,{useEffect, useState} from "react";
+import {View,Text,TouchableOpacity, Button, FlatList} from 'react-native';
 import { storage } from "../../hook/useStore";
 import RNCalendarEvents from "react-native-calendar-events";
 import { Popup } from "./modal";
+import { styles } from "./reminders.styles";
 
+const Item = ({item}) => {
+  return (
+    <View style = {styles.container}>
+    <TouchableOpacity style = {styles.textContainer}>
+      <Text style = {styles.text}>{item.title}</Text>
+    </TouchableOpacity>
+  </View>
+  );
+};
 
 //fetch keys from storage and then render a list of title time pairs. Onclick function is a popup which takes argument, the event id. Popup will display all the info using textInput.
 //When the save button pressed, it calls saveEvent function with optional argument ID. 
 
 //When create new event button is pressed, it opens up the popup without an argument Id, leaving all the fields empty. When saved, it calls saveEvent without the argument, creating a new event instead.
 
-let data = storage.getBuffer("keys"); //won't work.. data is limited..
-//data = data.map((item)=>JSON.parse(storage.get(item)));
 
 //set recurrence to be weekly and create different events for even a single alarm
 
 const Reminders = () => {
-  storage.delete("data");
+  const [data,setData] = useState(0);
+  useEffect( () => {
+    let data = storage.getString('keys'); 
+    data = data.split(',');
+    data = data.map(item => JSON.parse(storage.getString(item)));
+    setData(data);
+  },[]);
+  
   return(
-   <>
-      <Text>{storage.contains('data')?storage.getString("data"):"Key data does not exist"}</Text>
-    </>
+   <View >
+    {data?(
+      <FlatList
+      data = {data}
+      renderItem={({item}) => (
+        <Item item = {item}/>
+      )}
+      keyExtractor={(item) => item}
+      />
+    ):<Text>No data yet</Text>
+    }
+   </View>
   )
 }
 
---
+++ components/reminders/reminders.styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+import { COLORS,FONTS,SIZES,SHADOWS } from "../../constants";
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    margin:5,
+    marginLeft:5,
+    marginRight:5,
+    flexDirection:'row',
+    alignContent:'space-between',
+  },
+
+  textContainer: {
+    flex : 5,
+    padding:15,
+    margin:5,
+    borderRadius:5,
+    backgroundColor:COLORS.secondary,
+  },
+
+  text: {
+    fontSize:SIZES.medium,
+    color:COLORS.white,
+    fontFamily:'monospace'
+  }
+
+})
+
+export {styles};
